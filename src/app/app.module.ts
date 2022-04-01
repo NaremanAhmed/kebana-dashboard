04c33d8ec49f7d14f6d4545984373268b25701a0
NaremanAhmed <nareman.mokhtar@outlook.com>
2022-04-01 19:41:00 +0200

    add design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DialogComponent } from './dialog/dialog.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    DialogComponent
+    DialogComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
+    FontAwesomeModule, 
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
